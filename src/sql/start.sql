f29c03ba1dbe34f6c48ee373a700799e2f231279
George Kartavenko <socialimp@mail.ru>
2026-03-30 21:32:42 +0300

    :feat: add 5 exersices

--- src/sql/start.sql
+++ src/sql/start.sql
@@ -23,3 +23,22 @@ INSERT INTO employees (name, position, salary) VALUES
 
 -- 5. Смотрим результат
 SELECT * FROM employees;
+
+SELECT name, position FROM employees
+WHERE name like '%е%'
+
+
+-- Посчитать общее количество сотрудников, у которых зарплата меньше 70000.
+
+select count(*) from employees
+where salary < 70000
+
+
+-- Показать имена и зарплаты сотрудников, отсортированных по алфавиту (A-Z).--
+select name, salary from employees
+ORDER BY name desc
+
+--Найти минимальную, максимальную и среднюю зарплату среди всех сотрудников.
+select min(salary) as min_salary, max(salary) as max_salary, avg(salary) as avg_salary from employees
+
+--Показать всех сотрудников, у которых дата найма (hire_date) не указана (NULL).
